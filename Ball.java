d6413ef0b2bd0f11c8fabf05c042f3daa117d031
ak-mawmaw <48893411+ak-mawmaw@users.noreply.github.com>
2019-04-20 13:26:22 -0400

    Create Ball.java

---
+++ Ball.java
@@ -0,0 +1,45 @@
+public class Ball {
+
+   private double x;
+   private double y;
+   private double vx = 1;
+   private double vy = 1;
+
+   public Ball(){
+      this.x = 50;
+      this.y = 0;
+      drawBall();  
+      move();
+   }
+
+   public void drawBall(){
+      
+      PennDraw.setPenRadius(2);
+      PennDraw.filledCircle(x, y, 2);
+   
+   }
+       
+
+   public void move() {
+      while (true){
+         if (PennDraw.hasNextKeyTyped() == true){ 
+            x += vx;
+            y += vy;
+            PennDraw.clear();
+            drawBall();
+            
+            if (x > 99 || x < 1)
+               vx= -vx;
+         
+            if (y > 99 || y < 1)
+               vy= -vy;
+          
+         }
+      }
+   }
+   public static void main(String[] args) {
+      PennDraw.setXscale(0,100);
+      PennDraw.setYscale(0,100);
+      Ball ball = new Ball();   
+   }
+}
