1249b107c39f3c33139040e6f74462097f375233
ak-mawmaw <48893411+ak-mawmaw@users.noreply.github.com>
2019-04-25 20:45:38 -0400

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,53 +1,30 @@
 public class Ball {
 
-   private double x;
-   private double y;
-   private double vx = 1;
-   private double vy = 1;
+   public double x;
+   public double y;
+   public double vx = 1;
+   public double vy = 1;
 
    public Ball(){
       this.x = 50;
       this.y = 0;
-      drawBall();  
-      move();
    }
-   
-   // getters for x and y 
-   public double getX(){
-      return this.x;
-   }
-         
-   public double getY(){
-      return this.y;
-   } 
-   
+
    public void drawBall(){
-      
+   
+      PennDraw.setXscale(0,100);
+      PennDraw.setYscale(0,100);
       PennDraw.setPenRadius(2);
+      PennDraw.setPenColor(PennDraw.BLACK);
       PennDraw.filledCircle(x, y, 2);
    
    }
-     
-   public void paddleCheck(Paddle p){
-   // this will be a helper method and ill add this to move
-   //this function will calculate based on the center of the paddle
-   // set level in paddle we might have to change the math based on l  
-       
-    int w = p.getL();    
-      if ((this.x >= (p.getX() - .5*w)) && (this.x <= (p.getX() + .5*w))
-         && (this.y <= p.getY())){
-            this.vy= -this.vy;  
-      }
-   }
-
        
 
    public void move() {
-      while (true){
-         if (PennDraw.hasNextKeyTyped() == true){ 
+      PennDraw.enableAnimation(30);         
             x += vx;
             y += vy;
-            PennDraw.clear();
             drawBall();
             
             if (x > 99 || x < 1)
@@ -56,12 +33,13 @@ public class Ball {
             if (y > 99 || y < 1)
                vy= -vy;
           
-         }
-      }
+          PennDraw.advance();
+      
+      
    }
    public static void main(String[] args) {
-      PennDraw.setXscale(0,100);
-      PennDraw.setYscale(0,100);
-      Ball ball = new Ball();   
+      Ball ball = new Ball();
+      while (true)
+      ball.move();   
    }
 }
