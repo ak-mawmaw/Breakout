16ed0e015da9139660193e1e53536b8a79b85228
yaaowusu00 <49773006+yaaowusu00@users.noreply.github.com>
2019-04-27 14:22:31 -0400

    Update Paddle.java

--- Paddle.java
+++ Paddle.java
@@ -1,60 +1,66 @@
-public class Paddle{
-// x , y are the coordinates of the left side of the bat
-   public double x;
-   public double y = 0;
-   public int level;
-   public int width = 10 - (2 * level);
-   
-   public Paddle(double x, double y){
-      this.x = x;
-      this.y = y;
-      this.level = level;
-   }
-    public double getX(){
-      return this.x;
-   }
 
-   public double getY(){
-      return this.y;
-   } 
+   public class Paddle{
+   // x , y are the coordinates of the left side of the bat
+      private double x;
+      private double y = 0;
+      private int level;
+      public int width = 10 - level; 
+      
+      public Paddle(double x, double y, int level){
+         this.x = x;
+         this.y = y;
+         this.level = level;
+      }
+        public double getX(){
+      return this.x;
+       }
+
+      public double getY(){
+         return this.y;
+      } 
+      
+      public int getW(){
+         return this.width;
+      } 
    
-   public int getL(){
-      return this.level;
-   } 
-   
-   
-   public void draw(){
-    PennDraw.clear();
-      PennDraw.setXscale(0,100);
-      PennDraw.setYscale(0,100);
-      PennDraw.setPenColor(PennDraw.RED);
-      PennDraw.setPenRadius(0.02);
-      PennDraw.line(x - width, y, x +  , y);
-   }
-   
-   //moves the bat along the screen
-   public void move(){
-      char k;
-      String s;
-      draw();
-      PennDraw.enableAnimation(30);
-      if (PennDraw.hasNextKeyTyped() == true){
-         k = PennDraw.nextKeyTyped();
-         s = String.valueOf(k);
-         if ((s.equals("l") || s.equals("L") )&& x != 90){
-            x+= 10;
-            draw();
-         }
-         else if ((s.equals("j") || s.equals("J")) && x != 10){
-            x-= 10; 
-            draw();
-         }
-         PennDraw.advance();
+      
+      public void draw(){
+      PennDraw.clear();
+         PennDraw.setXscale(0,100);
+         PennDraw.setYscale(0,100);
+         PennDraw.setPenColor(PennDraw.RED);
+         PennDraw.setPenRadius(0.02);
+         PennDraw.line(x - width , y, x + width, y);
       }
       
+      //moves the bat along the screen
+      public void move(){
+         char k;
+         String s;
+         draw();
+         PennDraw.enableAnimation(30);
+         if (PennDraw.hasNextKeyTyped() == true){
+            k = PennDraw.nextKeyTyped();
+            s = String.valueOf(k);
+            if ((s.equals("l") || s.equals("L") )&& x != 90){
+               x+= 10;
+               draw();
+            }
+            else if ((s.equals("j") || s.equals("J")) && x != 10){
+               x-= 10; 
+               draw();
+            }
+            PennDraw.clear();
+            PennDraw.advance();
+         }
+         
+      }
+   public static void main(String[] args){
+      Paddle paddle = new Paddle(50, 0, 0 );
+      while (true)
+      paddle.move();
    }
- 
-}
-
+   
+   }
 
 
