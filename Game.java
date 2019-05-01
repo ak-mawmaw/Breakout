8563a54101ecd84fbcd0bbadab9b2021264523fd
ak-mawmaw <48893411+ak-mawmaw@users.noreply.github.com>
2019-05-01 12:00:01 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,69 +2,102 @@ public class Game{
    Ball ball;
    Paddle paddle; 
    Block block;
-  // Node head;
- //  Node last;
+   Node first;
+   boolean hasWon = false;
 
-   public Game (Ball ball, Paddle paddle){
+   
+  
+
+   public Game (Ball ball, Paddle paddle, int a){
+      PennDraw.clear(PennDraw.BLACK);
+      PennDraw.setXscale(0,100);
+      PennDraw.setYscale(0,100);
       this.ball = ball;
       this.paddle = paddle; 
-      head = new Node(block, null);
-      last = new Node(block);
+      this.first = new Node(0,0,null);
       
-   }
+      
+      Node[] node = new Node[18];
+      Node last = new Node(95, a, null);
+      Node head = new Node(5, a, last);
+      first.next = head;
+   
+      int x = 15;
+      int y = 96;
+      int i = 0;
+      
+      for (Node n = head; n.next!= null; n = n.next){
+         if (i != 8) { 
+            node[i] = new Node(x, y, null);
+            n.insertNode(node[i]);
+            x+=10;
+            i++;  
+         }
+      
+         
+         if(first.recursiveSize() == 1)
+            hasWon = true;
+      }
+         
+      while (hasWon == true){
+         PennDraw.disableAnimation();
+         PennDraw.clear();
+         PennDraw.setPenColor(PennDraw.BLACK);
+         PennDraw.setFontSize(20);
+         PennDraw.setFontBold();
+         PennDraw.text(50, 50, "YOU WIN !!! :(");
+      }
+   
+          
+   } 
+     
+   
    // call new node make recursive call 
    // we might have to buld the nodes backwards and the one it takes in will be the 
    //next 
-   public void build(int a){ // a is number of rows 
-   
-     //* Node k = new Node(block);
-      if (last == null)
-         head.next = last;
-      
-      else{
-         PennDraw.enableAnimation(30);
-         if (a == 0)
-            return;
-         for (Node n = head; n.next!= null; n = n.next){
-            int i = 0;
-            int y = 96;
-            Block [] b = rowOfBlocks(y); 
-            k = new Node(b[i], n.next);
-            n.insertBlock(k);
-            if (i == 9){
-               y-=4;
-               i = 0;
-               a--;
-            }
+   public void build(int a){ // a is y cordinate of row
+     // PennDraw.enableAnimation(30);
                
-            i++;
-            PennDraw.advance();
-         }
-      
+      if (ball.getY() >= 92){
+         Node k = first.isHit(ball.getX(), ball.getY());
+         ball.bounceUp();
+         //System.out.println(k);
+         first.deleteNode(k); 
+         //System.out.println(first);
+       
+         block = new Block(first);
+      //block.draw(head);
+         block.draw(first.next);
+      //PennDraw.advance();
       }
    }
-// if the x coordinate of the block is 95, end,
-
-   
+         
 
    // this method actually runs the game.
-   // tha ball, paddle and blocks are made to interact here. 
+   // the ball, paddle and blocks are made to interact here. 
    public void simulate(){
-      PenDraw.clear(0,0,0)
-      PennDraw.setXscale(0,100);
-      PennDraw.setYscale(0,100);  
-      while (ball.getX() <= 100 && paddle.getY() <=100){
-         //PennDraw.setXscale(0, 100);
-         //PennDraw.setYscale(0,100);
+     
+      while (ball.getX() <= 100 && paddle.getY() <=100 && ball.getY() != 0){
          paddle.move();
          if (ball.getY() < 5) paddleCheck(); 
          ball.move();
          
+         
          PennDraw.enableAnimation(30); 
-         PennDraw.clear();
-         ball.drawBall();
+         PennDraw.clear(PennDraw.BLACK);
+         build(96);
+         //block.draw(first.next);
+         ball.drawBall(); 
          paddle.draw();
          PennDraw.advance();
+      }
+      PennDraw.disableAnimation();
+      if (ball.getY() == 0){
+         PennDraw.clear();
+         PennDraw.setPenColor(PennDraw.BLACK);
+         PennDraw.setFontSize(20);
+         PennDraw.setFontBold();
+         PennDraw.text(50, 50, "YOU LOSE !!! :(");
       }
    }
 
@@ -75,24 +108,18 @@ public class Game{
       int w = paddle.getW();
       if ((ball.getX() >= (paddle.getX() - w)) && (ball.getX() <= (paddle.getX() + w))
          && (ball.getY() <= 5)){
-         ball.bounce();  
+         ball.bounceUp();  
       }
    }
-   // we will check the height of the ball before we call this, not inside
-   // cchange this to check with the nodes, not a block 
-   public void blockCheck(){
-      if (block.getX() >= block.getLc() && (ball.getX() <= block.getRc()) && (ball.getY() 
-      >= block.getBy()) && (ball.getY() <= block.getTy())) {
-         ball.bounce(); 
-      }
-   
-   }
+  
+
 
    public static void main(String[] args){
       Ball baller = new Ball();
       Paddle paddle = new Paddle(50,0,0); 
-      Game game = new Game(baller, paddle);
+      Game game = new Game(baller, paddle, 96);
+      //Game gamer = new Game(baller, paddle, 91);
+      
       game.simulate();
-      game.build(10);
    }
 }
