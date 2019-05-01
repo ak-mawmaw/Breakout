797dc046af2adec9fe6a2507f15b792cf9c5899c
ak-mawmaw <48893411+ak-mawmaw@users.noreply.github.com>
2019-04-30 20:34:55 -0400

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,76 +1,67 @@
 public class Block{
-    //instance variables 
-   private int x;
-   private int y;
-   private int xlc = x - 5; // because full width of block is ten 
-   private int xrc = x + 5; 
-   private int ytc = y + 2; // because full height of block is 4 
-   private int ybc = y - 2;
-
-    // getters for left corner and right corner
-   public int getLc() { 
-      return this.xlc;
-   }
-
-   public int getRc() { 
-      return this.xrc;
-   }
-
-    //getters for top y coordinate and bottom y coordinate of block 
-   public int getTy() { 
-      return this.ytc;
-   }
-
-   public int getBy() { 
-      return this.ybc;
-   }
-   public int getX(){
-      return x;
-   }
-    
-   public int getY(){
-      return y;
-   }
+   private Node n;
 
     //constructor 
-   public Block(int x, int y){
-      this.x = x;
-      this.y = y;
-   }
-   
-   public void draw(int x, int y){
-   
-      PennDraw.setXscale(0, 100);
-      PennDraw.setYscale(0, 100);
-      double z = Math.random();
-      if (z < .33 ) { 
-         PennDraw.setPenColor(PennDraw.GREEN);
-         PennDraw.filledRectangle (x, y, 5, 2);
-      } 
-   
-      if (z < .66 && z >= .33 ) {   
-         PennDraw.setPenColor(PennDraw.RED);
-         PennDraw.filledRectangle (x, y, 5, 2);
-      }
-   
-      if (z >= 0.66 && z < 1) {
-         PennDraw.setPenColor(PennDraw.YELLOW);
-         PennDraw.filledRectangle (x, y, 5, 2);
-      }
-   
-   
-   }
-   public String toString(){
-      String s = "";
-   
-      s += x + " " + y + " ";
-   
-      return s; 
-   }
-   
+    //blocks take in nodes that contain the center coordinates
+    //of the block
+   private Block(Node n){
+      this.n = n;
+    
 
-   public static void main(String[] args){
-      Block block = new Block(50, 50);
-      System.out.println(block);
+   public ( Node n) // getter
+      PennDraw.setYscale(0, 100);
+   }
+   
+   //draw the blocks recursively
+   public void draw(Node n){
+      if (n.next == null)
+         return;
+      
+      else{
+         double z = Math.random();
+         if (z < .33 ) { 
+            PennDraw.setPenColor(PennDraw.GREEN);
+            PennDraw.filledRectangle (n.getX(), n.getY(), 4, 2);
+         } 
+      
+         if (z < .66 && z >= .33 ) {   
+            PennDraw.setPenColor(PennDraw.RED);
+            PennDraw.filledRectangle (n.getX(), n.getY(), 4, 2);
+         }
+      
+         if (z >= 0.66 && z < 1) {
+            PennDraw.setPenColor(PennDraw.YELLOW);
+            PennDraw.filledRectangle (n.getX(), n.getY(), 4, 2);
+         }
+        
+      
+      }
+      
+      draw(n.next);
+   }   
+
+      public static void main(String[] args){
+      Node[] node = new Node[10];
+      Node last = new Node(95, 96, null);
+      Node head = new Node(5, 96, last);
+      int x = 15;
+      int y = 96;
+      int i= 0;
+     
+      
+      for (Node n = head; n.next!= null; n = n.next){
+         if (i != 8) { 
+            node[i] = new Node(x, y, null);
+            n.insertNode(node[i]);
+            x+=10;
+            i++; 
+         }
+      }
+      
+      
+      Block b = new Block(head);
+      b.draw(head);
+      
+      
    } 
 }
